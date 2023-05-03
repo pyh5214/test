ecec653ce0de06db429e7d225c6280ecd4f2299e
pyh5214 <70250251+pyh5214@users.noreply.github.com>
2023-05-03 23:08:19 +0900

    [unrated] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,7 @@
+def solution(cards1, cards2, goal):
+    for target in goal:
+        if len(cards1) and target == cards1[0]: cards1.pop(0)
+        elif len(cards2) and target == cards2[0]: cards2.pop(0)
+        else: return 'No'
+    print('gogo')
+    return 'Yes'
